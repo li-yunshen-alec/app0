c6d59538b9506dd650c73b02c500f12d2386584d
Alec Li <yunshen08@gmail.com>
2024-07-25 21:58:18 -0400

    firebase auth integration

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,14 +1,97 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import { images } from '../../constants'
+import FormField from '../../components/FormField'
+
+import CustomButton from '../../components/CustomButton'
+import { Link, router } from 'expo-router'
+import { getCurrentUser, signIn } from '../../lib/appwrite'
+import { auth } from '../../lib/firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
+    
+    setIsSubmitting(true);
+    
+    try {
+      signInWithEmailAndPassword(auth, form.email, form.password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+
+      Alert.alert("Success", "User signed in successfully");
+      router.replace('/home');
+    } catch (error) {
+      Alert.alert('Error', error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+        <View className='w-full justify-center min-h-[85vh] px-4 my-6'>
+          <Image 
+            source={images.logo}
+            className='w-[115px] h-[35px]'
+            resizeMode='contain'
+          />
+
+          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>
+            Log in
+          </Text>
+
+          <FormField 
+            title='Email'
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles='mt-7'
+            keyboardType='email-address'
+          />
+          <FormField 
+            title='Password'
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles='mt-7'
+          />
+
+          <CustomButton 
+            title='Sign in'
+            handlePress={submit}
+            containerStyles='mt-7'
+            isLoading={isSubmitting}
+          />
+
+          <View className='justify-center pt-5 flex-row gap-2'>
+            <Text className='text-lg text-gray-100 font-pregular'>
+              Don't have an account?
+            </Text>
+            <Link href={'/sign-up'} className='text-lg font-psemibold text-secondary'>Sign up</Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default SignIn
-
-const styles = StyleSheet.create({})
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,14 +1,117 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import { images } from '../../constants'
+import FormField from '../../components/FormField'
+
+import CustomButton from '../../components/CustomButton'
+import { Link, router } from 'expo-router'
+import { auth, db } from '../../lib/firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { doc, setDoc } from 'firebase/firestore'
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
+    
+    setIsSubmitting(true);
+    
+    try {
+      createUserWithEmailAndPassword(auth, form.email, form.password)
+        .then((userCredential) => {
+          // Signed up
+          const signUp = async () => {
+            const user = userCredential.user;
+            
+            const userDocRef = doc(db, "users", user.uid);
+
+            const userData = {
+              username: form.username,
+              habits: {}
+            };
+            
+            await setDoc(userDocRef, userData);
+          }
+
+          signUp();
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          
+          console.log(error);
+        });
+
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+        <View className='w-full justify-center min-h-[85vh] px-4 my-6'>
+          <Image 
+            source={images.logo}
+            className='w-[115px] h-[35px]'
+            resizeMode='contain'
+          />
+
+          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>
+            Sign up
+          </Text>
+
+          <FormField 
+            title='Username'
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles='mt-7'
+          />
+          <FormField 
+            title='Email'
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles='mt-7'
+            keyboardType='email-address'
+          />
+          <FormField 
+            title='Password'
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles='mt-7'
+          />
+
+          <CustomButton 
+            title='Sign up'
+            handlePress={submit}
+            containerStyles='mt-7'
+            isLoading={isSubmitting}
+          />
+
+          <View className='justify-center pt-5 flex-row gap-2'>
+            <Text className='text-lg text-gray-100 font-pregular'>
+              Have an account already?
+            </Text>
+            <Link href={'/sign-in'} className='text-lg font-psemibold text-secondary'>Log in</Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default SignUp
-
-const styles = StyleSheet.create({})
