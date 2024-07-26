289a08fd07ee9e7ad8a0d7e91d8d9ae2eac710a6
Alec Li <yunshen08@gmail.com>
2024-07-26 00:19:47 -0400

    first lesson

---
+++ data/index.js
@@ -0,0 +1,122 @@
+import { images } from "../constants";
+
+export const lessonData = [
+  {
+    id: 1,
+    title: 'Setting small goals for big results',
+    content: [
+      {
+        isCover: true,
+        title: `The power of taking small steps for big results`,
+        content: ``,
+        image: 'https://media1.tenor.com/m/gIbU3WxDOPUAAAAd/cat-jump-tabby-cat.gif'
+      },
+      {
+        title: '',
+        content:
+`It may seem obvious that to lose weight, you need to take set goals and take action.
+
+But studies have shown that many people who try to lose weight don't actually set goals.`,
+      },
+      {
+        title: '',
+        content:
+`This is one of the greatest obstacles to weight loss - studies have shown that when people don't set goals, they tend to avoid taking action.
+
+It's extremely easy to say "I'll start tomorrow" when you never made plans to start today.`,
+      },
+      {
+        title: '',
+        content:
+`Setting goals means `,
+      },
+      {
+        title: 'The fallacy of "Thinking big"',
+        content:
+`You may be inclined to set crazy goals in order to achieve big results.
+
+For example, you might say that you'll never eat ice cream again.`,
+        image: 'https://media1.tenor.com/m/IwjS55pNya4AAAAd/cat-licking-an-ice-cream-cat.gif'
+      },
+      {
+        title: 'The fallacy of "Thinking big"',
+        content:
+`The reality is, crazy goals only lower your chances of success, because they often create a huge gap between what you intend to do and what you actually do.
+
+Would you really be able to never eat ice cream again?`
+      },
+      {
+        title: 'The fallacy of "Thinking big"',
+        content:
+`Studies show that setting smaller, achievable goals is more effective.
+
+This is because of the principle of positive reinforcement, which states that rewarding behaviours are more likely to be repeated.`
+      },
+      {
+        title: 'The power of achievable goals',
+        content:
+`As you accomplish small goals in quick succession, the proudness you feel with each accomplishment makes you more likely to repeat the behaviour.
+
+For instance, if your goal is to replace your usual snack with a banana each day, you can start by replacing the snack once every three days.`
+      },
+      {
+        title: 'The power of achievable goals',
+        content:
+`On Monday, if you eat a banana you can feel proud of yourself. This makes you more likely to do the same in the future.
+
+When you do the same on Thursday, you're further reinforcing the behaviour. This creates the fuel you need to advance further with your goal.
+
+Once you're comfortable with once every three days, you can go to once every two days, and once you're ready, you can completely replace your snack with bananas. 
+
+All because you reinforced your behaviour by accomplishing achievable goals.`
+      },
+      {
+        title: 'A framework for setting effective goals',
+        content:
+`1. Set specific goals. The more specific, the better. For example, "I will go for a walk at 5:30 a.m. after brushing my teeth."
+
+This works because it gets you thinking about HOW you'll achieve a goal. You're much more likely to go for a walk when you've decided where and when you'll do it.`
+      },
+      {
+        title: 'A framework for setting effective goals',
+        content:
+`With a specific goal, you'll know exactly when you've achieved it.
+
+This results in easier reinforcement of behaviour - because you can feel proud of yourself at the specific point that you've achieved a specific goal.`
+      },
+      {
+        title: 'A framework for setting effective goals',
+        content:
+`2. Set truly achievable goals. For example, rather than trying to quit something cold-turkey, start by removing it from your life once a week.
+
+Once you're comfortable with once, a week, you can try twice a week. Again, this approach makes your more likely to achieve your desired behaviour because you're reinforcing it through constant accomplishment.`
+      },
+      {
+        title: 'A framework for setting effective goals',
+        content:
+`3. Make your goals behaviour-based rather than outcome-based
+
+A behaviour based goal may be, "I will eat between 2,000 and 2,200 calories each day", while an outcome-based goal would be "I will lose 1 pound this week."`
+      },
+      {
+        title: 'A framework for setting effective goals',
+        content:
+`Ultimately, it is better to set behaviour-based goals because achieving these goals is entirely in your control.
+
+For example, there are many things that can affect the amount of weight you hold. Humidity, water intake, sodium levels, time since eating, etc.
+
+So in the short-term, it's very difficult to control the amount of weight lost, which can lead to frustration and disappointment.`
+      },
+      {
+        title: 'A framework for setting effective goals',
+        content:
+`On the other hand, studies have shown that setting behavioural goals actually leads to greater likelihood of achieving desired outcomes.
+
+This is because it is changes in behaviours that drive changes in outcomes.
+
+So when we focus on changing our behaviours by setting achievable, behaviour-based goals, we make the fastest progress towards achieving desired outcomes.`
+      },
+    ]
+  },
+]
+
